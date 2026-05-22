5dc5ceff2ee3a14320f0435adc1c872ef8835b9c
Diana Murtazina <murka8831@yandex.ru>
2026-05-22 15:29:33 +0300

    added import new rule

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const pathChecker = require("./lib/rules/path-checker.js");
 const publicApiImports = require("./lib/rules/public-api-imports.js");
+const layerImports = require("./lib/rules/layer-imports.js");
 
 module.exports = {
   rules: {
     "path-checker": pathChecker,
-    "public-api-imports": publicApiImports
+    "public-api-imports": publicApiImports,
+    "layer-imports": layerImports
   }
 };
